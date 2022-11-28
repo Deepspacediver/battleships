2c4629189700dfda14e3a1ae421df1357e7c2c5f
deepspacediver <luka.koscik@gmail.com>
2022-11-28 20:26:42 +0100

    added babel and jest

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,16 @@ module.exports = {
         type: "asset/resource",
         exclude: /node_modules/,
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
 };
