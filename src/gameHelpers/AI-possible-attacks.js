b1f07c71b808ddeb258faf74f8c78ff52442c207
deepspacediver <luka.koscik@gmail.com>
2022-12-01 21:18:00 +0100

    helper functions for AI

---
+++ src/gameHelpers/AI-possible-attacks.js
@@ -0,0 +1,20 @@
+const createArrayOfAttacks = () => {
+  const attacks = [];
+
+  for (let i = 0; i < 10; i++) {
+    let xCoordinate = i;
+    for (let j = 0; j < 10; j++) {
+      let yCoordinate = j;
+      attacks.push([xCoordinate, yCoordinate]);
+    }
+  }
+  return attacks;
+};
+
+const possibleAttacks = createArrayOfAttacks();
+
+const getValidAttack = (attacksArray) =>
+  attacksArray.splice(Math.floor(Math.random() * attacksArray.length), 1);
+  
+export default possibleAttacks;
+export { getValidAttack };
