df337e55cdcd87008509dce05921352c4fed92cf
deepspacediver <luka.koscik@gmail.com>
2022-12-04 08:57:41 +0100

    switched x and y coordinates

--- src/gameHelpers/AI-possible-attacks.js
+++ src/gameHelpers/AI-possible-attacks.js
@@ -2,9 +2,9 @@ const createArrayOfAttacks = () => {
   const attacks = [];
 
   for (let i = 0; i < 10; i++) {
-    let xCoordinate = i;
+    let yCoordinate = i;
     for (let j = 0; j < 10; j++) {
-      let yCoordinate = j;
+      let xCoordinate = j;
       attacks.push([xCoordinate, yCoordinate]);
     }
   }
