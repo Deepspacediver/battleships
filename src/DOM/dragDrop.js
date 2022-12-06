dba24cf637698b86629717260c86e9571f30bf04
deepspacediver <luka.koscik@gmail.com>
2022-12-06 12:14:07 +0100

    button and drag events

--- src/DOM/dragDrop.js
+++ src/DOM/dragDrop.js
@@ -2,7 +2,8 @@ import gameHandler from "../modules/gameHandler";
 
 const boardForPlacement = document.getElementById("place-ship-board");
 const shipsContainer = document.getElementById("ships-container");
-
+const startBtn = document.querySelector("#start");
+const resetBtn = document.querySelector("#reset");
 const ships = document.querySelectorAll(".ship");
 
 const generateShipBlocks = () => {
@@ -18,20 +19,41 @@ const generateShipBlocks = () => {
 };
 generateShipBlocks();
 
-const myGameHandler = gameHandler("test");
+let myGameHandler = gameHandler("test");
+
+startBtn.addEventListener("click", () => {
+  console.log(myGameHandler.canStartGame());
+});
+
+resetBtn.addEventListener("click", () => {
+  myGameHandler = gameHandler("test");
+
+  classRemoval("anchored", boardForPlacement);
+  classRemoval("removed", shipsContainer);
+});
+
+shipsContainer.addEventListener("dragstart", (e) => {
+  e.dataTransfer.setData("ship-length", e.target.dataset.length);
+  e.dataTransfer.setData("ship-name", e.target.id);
+});
 
 boardForPlacement.addEventListener("dragover", (e) => {
-  if (e.target.id === "place-ship-board") return;
-  e.target.style.backgroundColor = "purple";
+  if (
+    e.target.id === "place-ship-board" ||
+    e.target.classList.contains("anchored")
+  )
+    return;
+  e.target.classList.add("dragover");
   e.preventDefault();
 });
 boardForPlacement.addEventListener("dragleave", (e) => {
   if (e.target.id === "place-ship-board") return;
-  e.target.style.backgroundColor = "white";
+  e.target.classList.remove("dragover");
   e.preventDefault();
 });
 
 boardForPlacement.addEventListener("drop", (e) => {
+  e.target.classList.remove("dragover");
   const objectData = {
     length: Number(e.dataTransfer.getData("ship-length")),
     name: e.dataTransfer.getData("ship-name"),
@@ -40,11 +62,25 @@ boardForPlacement.addEventListener("drop", (e) => {
     Number(e.target.dataset.x),
     Number(e.target.dataset.y),
   ];
-  myGameHandler.anchorAShip(chosenCoordinate, objectData);
+  const shipLocation = myGameHandler.anchorAShip(chosenCoordinate, objectData);
+  if (shipLocation) {
+    addAnchoredClass(shipLocation);
+    document.querySelector(`#${objectData.name}`).classList.add("removed");
+  }
 });
 
-shipsContainer.addEventListener("dragstart", (e) => {
-  e.dataTransfer.setData("ship-length", e.target.dataset.length);
-  e.dataTransfer.setData("ship-name", e.target.id);
-  console.log(e.dataTransfer);
-});
+const addAnchoredClass = (shipPlacement) => {
+  shipPlacement.forEach((placement) => {
+    const placementInDOM = boardForPlacement.querySelector(
+      `[data-x="${placement[0]}"][data-y="${placement[1]}"]`
+    );
+    placementInDOM.classList.add("anchored");
+  });
+};
+
+const classRemoval = (className, container = document) => {
+  const elementsToClear = Array.from(
+    container.querySelectorAll(`.${className}`)
+  );
+  elementsToClear.forEach((element) => element.classList.remove(className));
+};
