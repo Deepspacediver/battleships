ae0e1ee7c7fd0050e041d5c39a44981b63bbd7c2
deepspacediver <luka.koscik@gmail.com>
2022-12-13 12:20:25 +0100

    split webpack config into dev/prod/common

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[hash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
